16ea51f6c1bf8de51a26c890bfffb1817b4b073d
Franco C Sanchez <francocsanchez@gmail.com>
2022-07-31 11:18:50 -0300

    urlencode

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express(); // Inicio del servidor
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extends: true }));
 
 app.use(cors());
 
